b738dd4cbd3ae35205ee2804a5454e8c61a63af2
betymesfin <98676637+betymesfin@users.noreply.github.com>
2024-07-24 21:11:15 -0500

    useEffect,custom effect and fragment

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,29 @@ import * as React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+
+const useSemiPersistentState=()=>{
+  const OtodoList= JSON.parse(localStorage.getItem("savedTodoList")) ?? []
+  const[todoList,settodoList]= React.useState(OtodoList)
+  const StodoList= JSON.stringify(todoList)
+  React.useEffect(()=>{ 
+    localStorage.setItem("savedTodoList", StodoList)
+},[todoList])
+return[todoList,settodoList]
+}
+
+
 function App() {
-  const[todoList,settodoList]= React.useState([ ])
-    const addTodo = (newTodoItem) => {
+  const [todoList, settodoList]=useSemiPersistentState()
+   const addTodo = (newTodoItem) => {
     settodoList([...todoList, newTodoItem]);
-    
-  };
+    }
 return (
-   <div>
+   <>
      <h1>Todo List</h1>
      <AddTodoForm onAddTodo={addTodo}/>
      <TodoList todoList={todoList}/>
-   </div>
+   </>
 );
 }
 export default App
