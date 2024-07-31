c72446da598f57727b63148b7b0cdec5cfdaf92f
betymesfin <98676637+betymesfin@users.noreply.github.com>
2024-07-30 20:38:47 -0500

    Auto Focus and useRef

--- src/InputWithLabel.jsx
+++ src/InputWithLabel.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useRef } from "react"
+
 export function InputWithLabel(props){
+const InputRef= useRef()
+
+useEffect(()=>{
+    InputRef.current.focus()   
+})
+
     return(
         <>
         <label htmlFor="todoTitle">{props.children}</label>
-        <input value={props.todoTitle} onChange={props.handleTitleChange} id="todoTitle" name="todoTitle"/>
+        <input value={props.todoTitle} onChange={props.handleTitleChange} id="todoTitle" name="todoTitle"
+        ref={InputRef} />
         </>
     )
 }
